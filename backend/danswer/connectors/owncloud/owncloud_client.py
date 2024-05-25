4f30faddc40947a97c2f96ff791401db7adad05f
Bijay Regmi <bijayregmi5@gmail.com>
2024-05-25 17:13:29 +0200

    change file depth and apply sorting

--- backend/danswer/connectors/owncloud/owncloud_client.py
+++ backend/danswer/connectors/owncloud/owncloud_client.py
@@ -1,24 +1,25 @@
 import os
-import requests
-
 from dataclasses import dataclass
 from xml.etree import ElementTree
+
+import requests
+
 
 @dataclass(repr=True, eq=True)
 class OwnCloudFileObject:
     id: str
     file_name: str
 
+
 class OwnCloudClient:
     """OwnCloudClient is a class that interacts with OwnCloud API to fetch files and their content.
-    
+
     Attributes:
     base_url (str): The base URL of the OwnCloud instance.
     username (str): The username of the user.
     password (str): The password of the user.
     """
 
-    
     def __init__(self, base_url: str, username: str, password: str):
         if base_url.endswith("/"):
             base_url = base_url[:-1]
@@ -26,21 +27,29 @@ class OwnCloudClient:
         self.username = username
         self.password = password
 
-    def _dispatch_authenticated_request(self, method: str, url:str, **kwargs) -> requests.Response:
+    def _dispatch_authenticated_request(
+        self, method: str, url: str, **kwargs
+    ) -> requests.Response:
         """Dispatches a request to the OwnCloud API with the provided method and URL."""
         if url.startswith("/"):
             url = url[1:]
         url = f"{self.base_url}/{url}"
-        return requests.request(method=method, url=url, auth=(self.username, self.password), **kwargs)
-    
+        return requests.request(
+            method=method, url=url, auth=(self.username, self.password), **kwargs
+        )
+
     def is_folder(self, prop: ElementTree.Element) -> bool:
         resourcetype = prop.find("{DAV:}resourcetype")
         if resourcetype is None:
             return False
         return resourcetype.find("{DAV:}collection") is not None
-    
+
     def get_all_files(self) -> list[OwnCloudFileObject | None]:
-        response = self._dispatch_authenticated_request(method="PROPFIND", url=f"remote.php/dav/files/{self.username}", headers={"Depth": "1000000000"})
+        response = self._dispatch_authenticated_request(
+            method="PROPFIND",
+            url=f"remote.php/dav/files/{self.username}",
+            headers={"Depth": "100000000"},
+        )
         tree = ElementTree.fromstring(response.content)
         files = []
         for response in tree.findall("{DAV:}response"):
@@ -60,7 +69,7 @@ class OwnCloudClient:
             oc_obj = OwnCloudFileObject(id=etag.text, file_name=href)
             files.append(oc_obj)
         return files
-    
+
     def _convert_filename_relative_to_root(self, filename):
         if filename.startswith("/"):
             filename = filename[1:]
@@ -70,8 +79,13 @@ class OwnCloudClient:
         return f"{self.base_url}/apps/files/?dir={os.path.dirname(file_path)}"
 
     def get_file_content(self, file_path, stream=True):
-        response = self._dispatch_authenticated_request(method="GET", url=f"remote.php/dav/files/{self.username}/{file_path}", stream=stream)
+        response = self._dispatch_authenticated_request(
+            method="GET",
+            url=f"remote.php/dav/files/{self.username}/{file_path}",
+            stream=stream,
+        )
         return response.content
+
 
 if __name__ == "__main__":
     pass
